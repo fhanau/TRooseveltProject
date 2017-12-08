46ec5ca9a0776fdc12b3c74e09fbbfc71bb4e123
Felix Hanau <hanau2@illinois.edu>
2017-12-07 23:13:09 -0600

    Add image rendering

--- src/TRoosevelt.java
+++ src/TRoosevelt.java
@@ -1,7 +1,34 @@
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.util.Arrays;
 
 public class TRoosevelt {
+	static void mandelbrot(int[] buffer) {
+		Arrays.fill(buffer, 0x5FFFF);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		return;
+		System.out.println("TRoosevelt project by David Mitchell and Felix Hanau.");
+		System.out.println("Renders mandelbrot fractals.");
+
+        //Zen.create(320, 300, "stretch");
+		System.out.println("Zen ran (or not)");
+		int height = 450;
+		int width = 800;
+
+		int[] buffer = new int[height * width];
+		mandelbrot(buffer);
+		BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		for (int i = 0; i < bufferedImage.getWidth(); i++) {
+		    for (int j = 0; j < bufferedImage.getHeight(); j++) {
+		    	bufferedImage.setRGB(i, j, buffer[i * bufferedImage.getHeight() + j]);
+		    }
+		}
+
+		JFrame frame = new JFrame("Theodore Roosevelt Project");
+		frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(new JLabel(new ImageIcon(bufferedImage)));
+        frame.setVisible(true);
 	}
 }
